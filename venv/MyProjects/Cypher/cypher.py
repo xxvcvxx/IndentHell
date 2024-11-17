fda490b52d1a7538c18c4b25b2773d5e8c17c5c4
Olga <xvcvx.pl@gmail.com>
2024-11-18 00:35:45 +0100

    test

--- venv/MyProjects/Cypher/cypher.py
+++ venv/MyProjects/Cypher/cypher.py
@@ -1,7 +1,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input('Please enter "d" for decryption or "e" for encryption: ').lower()
+direction = input('Please enter "d" for decryption or "e" for encryption:  ').lower()
 while direction != 'e' and direction != 'd':
     print("Invalid input. Please enter only one character (e.g., 'd' or 'e').")
     direction = input('Please enter "d" for decryption or "e" for encryption: ').lower()
