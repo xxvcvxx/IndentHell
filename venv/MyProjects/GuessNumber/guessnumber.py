c099e6f0ef38683ce2fd92781d983d938d84b09c
Olga <xvcvx.pl@gmail.com>
2024-11-25 00:23:58 +0100

    refactor

--- venv/MyProjects/GuessNumber/guessnumber.py
+++ venv/MyProjects/GuessNumber/guessnumber.py
@@ -1,4 +1,5 @@
 import random
+import guessnumber_art
 
 random_number = random.randint(1, 100)
 
@@ -11,22 +12,24 @@ level = {
 def check_number(number1, random_number1):
     if random_number1 == number1:
         print("Congratulations! You've guessed the number.")
+        return True
     elif random_number1 > number1:
         print("Too low! Try again.")
     else:
         print("Too high! Try again.")
+    return False
 
 
+print(guessnumber_art.logo)
 user_choice = input("Enter your choice Easy(E), Medium(M), Difficult(D): ").capitalize()
 attempts = level[user_choice]
-number = None
 
 while attempts > 0:
     number = int(input("Guess a number: "))
-    check_number(number, random_number)
-    if number == random_number:
+    if check_number(number, random_number):
         break
     attempts -= 1
-    if attempts == 0:
+    if attempts > 0:
+        print(f"Attempts left: {attempts}")
+    else:
         print(f"You lost! The correct number was {random_number}.")
-    print(f"Attempts left: {attempts}")
