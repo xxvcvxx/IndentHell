f2572325e193cb2426fade41f0b30f716c2db1fc
Olga <xvcvx.pl@gmail.com>
2024-11-24 15:07:24 +0100

    Add: calculate score

--- venv/MyProjects/Blackjack/blackjack.py
+++ venv/MyProjects/Blackjack/blackjack.py
@@ -14,8 +14,33 @@ def draw_cards():
     return card
 
 
-choice = 'y'
-while choice == 'y':
-    print(draw_cards())
-    print(cards_in_game)
-    choice = input("again:")
+def calculate_score(list):
+    if 11 in list and sum(list) > 21:
+        index = list.index(11)
+        list[index] = 1
+        print("Ace is 1 now")
+    total = sum(list)
+    if total == 21:
+        return 0
+    elif total > 21:
+        return -1
+    else:
+        return total
+
+
+user_cards = [draw_cards(), draw_cards()]
+computer_cards = [draw_cards(), draw_cards()]
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+print(f"Your cards: {user_cards}")
+print(f"Computers cards: [{computer_cards[0]}, _]")
+
+if user_score == 0 or computer_score == 0 or user_score == -1:
+    print("END!")
+else:
+    choice = input("Y/N")
+    if choice == 'y':
+        user_cards.append(draw_cards())
+        calculate_score(user_cards)
