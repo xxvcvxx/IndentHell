754955cbc0dcda1b785ba7c5831dc44e48b19507
Olga <xvcvx.pl@gmail.com>
2025-03-09 23:34:50 +0100

    refactor

---
+++ venv/MyProjects/CoffeeMachine/coffeeMachine.py
@@ -0,0 +1,21 @@
+resources = {
+    "water": 1000,  # ml
+    "milk": 500,  # ml
+    "coffee": 200  # g
+}
+
+# Proporcje dla kaw
+recipes = {
+    "espresso": {
+        "water": 50,
+        "milk": 0,
+        "coffee": 18},
+    "latte": {
+        "water": 200,
+        "milk": 150,
+        "coffee": 24},
+    "cappuccino": {
+        "water": 250,
+        "milk": 100,
+        "coffee": 24}
+}
