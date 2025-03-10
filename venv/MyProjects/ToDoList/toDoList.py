d06d2155e6e9039584708904037477bf1ba05d20
Olga <xvcvx.pl@gmail.com>
2025-03-10 11:01:15 +0100

    To-Do List print task, mark task as done

--- venv/MyProjects/ToDoList/toDoList.py
+++ venv/MyProjects/ToDoList/toDoList.py
@@ -1,4 +1,4 @@
-tasks = {"id":1,"text":"task1"}
+tasks = {"done":False,"text":"task1"},{"done":False,"text":"task2"}
 
 def add_task():
     pass
@@ -7,9 +7,29 @@ def remove_task():
     pass
 
 def mark_task_as_done():
-    pass
+
+    while True:
+        try:
+            choice = int(input("Which task would you like to mark as done? "))
+
+            if 1 <= choice <= len(tasks):
+                tasks[choice - 1]["done"] = True
+                print(f"Task {tasks[choice - 1]['text']} marked as done!")
+                break
+            else:
+                print(f"Please enter a number between 1 and {len(tasks)}!")
+        except ValueError:
+            print("Error! Please enter a valid task number (a number).")
 
 def print_tasks():
- pass
+    for index, task in enumerate(tasks, start=1):
+        checkbox = "[√]" if task["done"] else "[ ]"
+        print(f"{index}. {checkbox} {task['text']}")
+
+
+mark_task_as_done()
+print_tasks()
+
+
 
 
