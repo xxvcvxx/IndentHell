9be9997100dc8c9960a01ad29959f15388418d72
Olga <xvcvx.pl@gmail.com>
2025-03-10 13:44:58 +0100

    To-Do List add_task,remove_task

--- venv/MyProjects/ToDoList/toDoList.py
+++ venv/MyProjects/ToDoList/toDoList.py
@@ -1,13 +1,24 @@
-tasks = {"done":False,"text":"task1"},{"done":False,"text":"task2"}
+tasks = [{"done": False, "text": "task1"}, {"done": False, "text": "task2"}]
 
 def add_task():
-    pass
+    text = input("New task: ")
+    tasks.append({"done": False, "text": text})
 
 def remove_task():
-    pass
+    while True:
+        try:
+            choice = int(input("Which task would you like to remove? "))
+
+            if 1 <= choice <= len(tasks):
+                print(f"Task {tasks[choice - 1]['text']} has been removed.")
+                del tasks[choice - 1]
+                break
+            else:
+                print(f"Please enter a number between 1 and {len(tasks)}!")
+        except ValueError:
+            print("Error! Please enter a valid task number (a number).")
 
 def mark_task_as_done():
-
     while True:
         try:
             choice = int(input("Which task would you like to mark as done? "))
@@ -26,10 +37,7 @@ def print_tasks():
         checkbox = "[√]" if task["done"] else "[ ]"
         print(f"{index}. {checkbox} {task['text']}")
 
-
+add_task()
+remove_task()
 mark_task_as_done()
 print_tasks()
-
-
-
-
