4fc59873737aa24b79e127b68e4d89931209c711
Olga <xvcvx.pl@gmail.com>
2025-03-12 23:23:59 +0100

    - Users can now choose between multiple task lists. - Added the ability to rename lists. - Lists can be modified and saved with new names.

--- venv/MyProjects/ToDoList_OOP/main.py
+++ venv/MyProjects/ToDoList_OOP/main.py
@@ -1,6 +1,22 @@
 from taskManager import TaskManager
 
 if __name__ == "__main__":
-    manager = TaskManager()
-    manager.show_tasks()
+    task_lists = {'1': 'toDo1.txt', '2': 'toDo2.txt', '3': 'toDo3.txt'}
 
+    print("Available task lists:")
+    for index, (key, filename) in enumerate(task_lists.items(), start=1):
+        print(f"{index}. {filename}")
+
+    file_path = ''
+    while True:
+        choice = input("Select a task list (1-3): ")
+        file_path = task_lists.get(choice)
+        if file_path:
+            break
+        print("Please enter a number between 1 and 3!")
+
+    manager = TaskManager(file_path)
+    manager.show_tasks()
+    manager.add_task()
+    manager.show_tasks()
+    manager.list_rename()
--- venv/MyProjects/ToDoList_OOP/taskManager.py
+++ venv/MyProjects/ToDoList_OOP/taskManager.py
@@ -1,9 +1,19 @@
+import os
 class TaskManager:
 
-    def __init__(self):
+    def __init__(self,filename):
+        self.filename = filename
         self.tasks = []
-        with open("toDo.txt", "r") as file:
-            for line in file:
+
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as file:
+                file.write("Unnamed List\n")
+            print("File not found. Created a new one.")
+
+        with open(filename, "r") as file:
+            lines = file.readlines()  # Creating a list of items
+            self.list_name = lines[0].strip()
+            for line in lines[1:]:  # Starting from the second line
                 text, done = line.strip().split(",")
                 self.tasks.append({"text": text, "done": done == "True"})
         print("File has been loaded.")
@@ -12,9 +22,28 @@ class TaskManager:
         text = input("New task: ")
         self.tasks.append({"done": False, "text": text})
         print("Task has been added.")
+        self.save_tasks_to_file()
+
     def show_tasks(self):
         print("\n--- Task List ---")
         for index, task in enumerate(self.tasks, start=1):
             checkbox = "[√]" if task["done"] else "[ ]"
             print(f"{index}. {checkbox} {task['text']}")
         print("-----------------\n")
+
+    def save_tasks_to_file(self):
+        with open(self.filename, "w") as file:
+            file.write(f"{self.list_name}\n")
+            for task in self.tasks:
+                file.write(f"{task['text']},{task['done']}\n")
+    print("File has been saved.")
+
+    def list_rename(self):
+        self.list_name = input("Enter new list name: ")
+
+        with open(self.filename, 'w') as file:
+            file.write(f"{self.list_name}\n")
+
+            for task in self.tasks:
+                file.write(f"{task['text']},{task['done']}\n")
+
