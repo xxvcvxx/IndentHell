bd9e951bdcd82d55884e7caa21a92809ca63bae8
Olga <xvcvx.pl@gmail.com>
2025-08-24 15:14:27 +0200

    snake game refactor

--- venv/MyProjects/Snake/main_snake.py
+++ venv/MyProjects/Snake/main_snake.py
@@ -8,22 +8,9 @@ screen.bgcolor("black")
 screen.title("MEGA SNAKE!")
 screen.tracer(0)
 
+snake_body = []
 
-snake_head = Turtle()
-snake_head.shape("square")
-snake_head.color("white")
-snake_head.penup()
-snake_head.goto(20,0)
-starting_position = (0,0)
-snake_body = [snake_head]
-
-
-for i in range (0,5):
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(starting_position)
-    snake_body.append(new_segment)
+snake = Snake()
 
 
 game_is_on = True
@@ -31,7 +18,7 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    Snake.snake_move(snake_body)
+    snake.snake_move()
 
 
 
--- venv/MyProjects/Snake/snake.py
+++ venv/MyProjects/Snake/snake.py
@@ -1,8 +1,22 @@
-class Snake:
+from turtle import  Screen, Turtle
 
-    def snake_move(snake_body):
-        for seg in range(len(snake_body)-1, 0, -1):
-            new_x = snake_body[seg - 1].xcor()
-            new_y = snake_body[seg - 1].ycor()
-            snake_body[seg].goto(new_x,new_y)
-        snake_body[0].forward(20)
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0), (-60, 0), (-80, 0)]
+class Snake:
+    def __init__(self):
+        self.snake_body= []
+        self.create_snake()
+
+    def create_snake(self):
+        for positions in starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(positions)
+            self.snake_body.append(new_segment)
+    def snake_move(self):
+        for seg in range(len(self.snake_body)-1, 0, -1):
+            new_x = self.snake_body[seg - 1].xcor()
+            new_y = self.snake_body[seg - 1].ycor()
+            self.snake_body[seg].goto(new_x,new_y)
+        self.snake_body[0].forward(20)
