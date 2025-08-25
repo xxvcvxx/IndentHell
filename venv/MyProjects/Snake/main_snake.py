a2da45677669a7338b6223792fa671ab7d6770a6
Olga <xvcvx.pl@gmail.com>
2025-08-25 17:45:18 +0200

    snake game food

--- venv/MyProjects/Snake/main_snake.py
+++ venv/MyProjects/Snake/main_snake.py
@@ -1,6 +1,7 @@
 import time
 from turtle import  Screen, Turtle
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(600,600)
@@ -9,18 +10,20 @@ screen.title("MEGA SNAKE!")
 screen.tracer(0)
 
 snake_body = []
-
 snake = Snake()
-
+food = Food()
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.05)
 
     snake.snake_move()
-
-
+    screen.onkey(snake.up,"Up")
+    screen.onkey(snake.down,"Down")
+    screen.onkey(snake.right,"Right")
+    screen.onkey(snake.left,"Left")
+    screen.listen()
 
 
 
--- venv/MyProjects/Snake/snake.py
+++ venv/MyProjects/Snake/snake.py
@@ -2,10 +2,16 @@ from turtle import  Screen, Turtle
 
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0), (-60, 0), (-80, 0)]
+move_distance = 20
+up = 90
+down = 270
+left = 180
+right = 0
 class Snake:
     def __init__(self):
         self.snake_body= []
         self.create_snake()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         for positions in starting_positions:
@@ -19,4 +25,21 @@ class Snake:
             new_x = self.snake_body[seg - 1].xcor()
             new_y = self.snake_body[seg - 1].ycor()
             self.snake_body[seg].goto(new_x,new_y)
-        self.snake_body[0].forward(20)
+        self.snake_body[0].forward(move_distance)
+    def up(self):
+        if self.head.heading() != down:
+            self.head.setheading(up)
+
+    def down(self):
+        if self.head.heading() != up:
+            self.head.setheading(down)
+
+    def right(self):
+        if self.head.heading() != left:
+            self.head.setheading(right)
+
+    def left(self):
+        if self.head.heading() != right:
+            self.head.setheading(left)
+
+
