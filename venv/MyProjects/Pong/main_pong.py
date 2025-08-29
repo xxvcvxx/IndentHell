9279850fdb34b2c5d4db007386502cfd3853e53c
Olga <xvcvx.pl@gmail.com>
2025-08-29 22:12:22 +0200

    Pong paddle

--- venv/MyProjects/Pong/main_pong.py
+++ venv/MyProjects/Pong/main_pong.py
@@ -10,9 +10,21 @@ screen.tracer(0)
 
 game_is_on = True
 
+
 left_paddle = Paddle('L')
-left_paddle = Paddle('R')
-screen.update()
+right_paddle = Paddle('R')
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    screen.update()
 
 
 
--- venv/MyProjects/Pong/paddle.py
+++ venv/MyProjects/Pong/paddle.py
@@ -4,6 +4,7 @@ starting_positions = {
     "R": [(350, 0), (350, 20), (350, 40)],
     "L": [(-350, 0), (-350, 20), (-350, 40)]
 }
+border = 260
 move_distance = 20
 up = 90
 down = 270
@@ -20,3 +21,16 @@ class Paddle:
             new_segment.penup()
             new_segment.goto(positions)
             self.paddle_segments.append(new_segment)
+
+    def up(self):
+        if self.paddle_segments[-1].ycor() + move_distance <= border:
+            for seg in self.paddle_segments:
+                new_y= seg.ycor() + move_distance
+                seg.goto(seg.xcor(),new_y)
+
+
+    def down(self):
+        if self.paddle_segments[0].ycor() - move_distance >= -border:
+            for seg in self.paddle_segments:
+                new_y= seg.ycor() - move_distance
+                seg.goto(seg.xcor(),new_y)
