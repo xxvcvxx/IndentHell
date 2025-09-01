ce320670fd06a81803b287bc642c56811dfe99d8
Olga <xvcvx.pl@gmail.com>
2025-09-01 23:04:12 +0200

    radCross init

---
+++ venv/MyProjects/RoadCross/player.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('black')
+        self.penup()
+        self.shape('turtle')
+        self.setheading(90)
+        self.goto(0,-280)
