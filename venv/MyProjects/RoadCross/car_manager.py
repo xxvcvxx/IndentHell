1777f21cce262bc6f63e2cb4681a2c170a54d68f
Olga <xvcvx.pl@gmail.com>
2025-09-16 01:13:34 +0200

    add menu

--- venv/MyProjects/RoadCross/car_manager.py
+++ venv/MyProjects/RoadCross/car_manager.py
@@ -10,8 +10,9 @@ class Car_manager(Turtle):
         self.shape("square")
         self.color("black")
         self.shapesize(stretch_wid=1, stretch_len=2)
-        r_choice=random.choice([300, -300])
-        self.goto(r_choice, 0)
+        #r_choice=random.choice([300, -300])
+        random_y = random.choice(range(260,-261,-20))
+        self.goto(300, random_y)
 
     def move(self):
         if self.xcor() == 300:
--- venv/MyProjects/RoadCross/main_roadCross.py
+++ venv/MyProjects/RoadCross/main_roadCross.py
@@ -12,13 +12,14 @@ car = Car_manager()
 
 screen.listen()
 screen.onkey(player.up, "Up")
-screen.onkey(player.down, "Down")
-screen.onkey(player.right, "Right")
-screen.onkey(player.left, "Left")
+#screen.onkey(player.down, "Down")
+#screen.onkey(player.right, "Right")
+#screen.onkey(player.left, "Left")
 
 game_is_on = True
-
+cars = [Car_manager() for _ in range(6)]
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
-    car.move()
+    time.sleep(0.05)
+    for car in cars:
+        car.move()
