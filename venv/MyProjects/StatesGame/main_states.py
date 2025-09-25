bb691984f0bd469d3e4f7f68c38591b5fe399fe5
Olga <xvcvx.pl@gmail.com>
2025-09-25 21:44:27 +0200

    StatesGame: init

--- venv/MyProjects/StatesGame/main_states.py
+++ venv/MyProjects/StatesGame/main_states.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import pandas
 
 screen = Screen()
 screen.bgpic("blank_states_img.gif")
@@ -6,8 +7,19 @@ screen.bgpic("blank_states_img.gif")
 pen = Turtle()
 pen.penup()
 pen.hideturtle()
-answer = screen.textinput("StatesGame", "Enter a state name")
-pen.write("blabla")
+data = pandas.read_csv("50_states.csv")
+
+game_is_on=True
+while game_is_on:
+    answer = screen.textinput("StatesGame", "Enter a state name").capitalize()
+    if answer in data.state.tolist():
+        row = data[data.state == answer]
+        x=int(row.x)
+        y=int(row.y)
+        pen.goto(x,y)
+        pen.write(answer)
+
+
 
 
 screen.exitonclick()
