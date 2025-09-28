2de6898a0f2012f64d4f69b904a1fd891d5bce69
Olga <xvcvx.pl@gmail.com>
2025-09-28 11:45:30 +0200

    Alphabet: refactor

---
+++ venv/MyProjects/NpAlphabet/alphabet_main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from prettytable import PrettyTable
+
+HEADERS = ["Letter", "NATO Code"]
+
+alphabet_data = pd.read_csv("nato_phonetic_alphabet.csv")
+table = PrettyTable()
+table.field_names = HEADERS
+word = input("Enter a word: ").upper()
+
+for letter in word:
+    for (index, row) in alphabet_data.iterrows():
+        if letter == row.letter:
+            table.add_row([letter, row.code])
+
+print(table)
+
+#2
+
+table2 = PrettyTable()
+phonetic_dict = {row.letter:row.code for (index,row) in alphabet_data.iterrows()}
+output_list = [phonetic_dict[letter] for letter in word]
+print(table2)
