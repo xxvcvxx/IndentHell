d20e800bbdfed47ecf017a5c7428152ed391d88d
Olga <xvcvx.pl@gmail.com>
2025-09-30 22:23:26 +0200

    Pomodoro : UI

---
+++ venv/MyProjects/Pomodoro/pomodoro_main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=50,pady=50,bg=YELLOW)
+
+canvas = Canvas(bg = YELLOW, highlightthickness = 0)
+image =PhotoImage(file="tomato.png")
+canvas.create_image(200,112,image=image)
+canvas.create_text(200,130, text="00:00",fill = "white", font=(FONT_NAME,24,"bold"))
+canvas.pack()
+
+
+window.mainloop()
