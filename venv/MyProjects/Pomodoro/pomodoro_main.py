570d71d309e9d916cf6f964b097ae7c79b901f21
Olga <xvcvx.pl@gmail.com>
2025-10-01 19:03:48 +0200

    Pomodoro : UI

--- venv/MyProjects/Pomodoro/pomodoro_main.py
+++ venv/MyProjects/Pomodoro/pomodoro_main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,8 +13,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+is_counting = False
+def start_timer():
+    global is_counting
+    if is_counting == False:
+        is_counting =True
+        count_down(5)
+def reset_timer():
+    global  is_counting
+    canvas.itemconfig(timmer_text,text=5)
+    is_counting=False
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    global is_counting
+    canvas.itemconfig(timmer_text,text=count)
+    if count >0 and is_counting==True:
+        window.after(1000,count_down,count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,8 +38,20 @@ window.config(padx=50,pady=50,bg=YELLOW)
 canvas = Canvas(bg = YELLOW, highlightthickness = 0)
 image =PhotoImage(file="tomato.png")
 canvas.create_image(200,112,image=image)
-canvas.create_text(200,130, text="00:00",fill = "white", font=(FONT_NAME,24,"bold"))
-canvas.pack()
+timmer_text= canvas.create_text(200,140, text="5",fill = "white", font=(FONT_NAME,24,"bold"))
+timer_label=Label(text="TIMER",fg = GREEN,bg=YELLOW, font=(FONT_NAME,24,"bold"))
+checkmark_label = Label(text="✓",fg = GREEN,bg=YELLOW, font=(FONT_NAME,30,"bold"))
+start_button=Button(text="Start",command=start_timer,fg = "white",bg=RED, font=(FONT_NAME,18,"bold"),activebackground="darkred")
+reset_button=Button(text="Reset",command=reset_timer,fg = "white",bg=RED, font=(FONT_NAME,18,"bold"),activebackground="darkred")
+
+
+start_button.grid(row=2, column=0)
+reset_button.grid(row=2, column=2)
+checkmark_label.grid(row=3,column=1)
+canvas.grid(row=1, column=1)
+timer_label.grid(row=0, column=1)
+
+
 
 
 window.mainloop()
