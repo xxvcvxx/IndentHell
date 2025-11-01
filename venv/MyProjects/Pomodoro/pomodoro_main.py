1028f25ef85b9760d6a1fbbb558b3ba9d13746e6
Olga <xvcvx.pl@gmail.com>
2025-11-01 15:52:59 +0100

    Pomodoro fixing timer

--- venv/MyProjects/Pomodoro/pomodoro_main.py
+++ venv/MyProjects/Pomodoro/pomodoro_main.py
@@ -6,32 +6,51 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 0.1
+SHORT_BREAK_MIN = 0.5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 is_counting = False
+reps = 0
+
 def start_timer():
-    global is_counting
-    if is_counting == False:
-        is_counting =True
-        count_down(5*60)
+    global is_counting,reps
+    is_counting = True
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    #if is_counting == False:
+    #    is_counting =True
+    #    count_down(WORK_MIN*60+10)
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+
 def reset_timer():
     global  is_counting
-    canvas.itemconfig(timmer_text,text=55)
     is_counting=False
+    canvas.itemconfig(timmer_text,text="RESET!")
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     global is_counting
     count_min = math.floor(count/60)
     count_sec = count % 60
 
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
     canvas.itemconfig(timmer_text,text=f"{count_min}:{count_sec}")
     if count >0 and is_counting==True:
         window.after(1000,count_down,count-1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -41,7 +60,7 @@ window.config(padx=50,pady=50,bg=YELLOW)
 canvas = Canvas(bg = YELLOW, highlightthickness = 0)
 image =PhotoImage(file="tomato.png")
 canvas.create_image(200,112,image=image)
-timmer_text= canvas.create_text(200,140, text="5",fill = "white", font=(FONT_NAME,24,"bold"))
+timmer_text= canvas.create_text(200,140, text="POMODORO!",fill = "white", font=(FONT_NAME,24,"bold"))
 timer_label=Label(text="TIMER",fg = GREEN,bg=YELLOW, font=(FONT_NAME,24,"bold"))
 checkmark_label = Label(text="✓",fg = GREEN,bg=YELLOW, font=(FONT_NAME,30,"bold"))
 start_button=Button(text="Start",command=start_timer,fg = "white",bg=RED, font=(FONT_NAME,18,"bold"),activebackground="darkred")
