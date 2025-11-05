d69b748d3bc5f6f712a4d44439d5620407c4271d
Olga <xvcvx.pl@gmail.com>
2025-11-05 23:19:47 +0100

    Add try/except

--- venv/MyProjects/NpAlphabet/alphabet_main.py
+++ venv/MyProjects/NpAlphabet/alphabet_main.py
@@ -6,18 +6,24 @@ HEADERS = ["Letter", "NATO Code"]
 alphabet_data = pd.read_csv("nato_phonetic_alphabet.csv")
 table = PrettyTable()
 table.field_names = HEADERS
-word = input("Enter a word: ").upper()
 
-for letter in word:
-    for (index, row) in alphabet_data.iterrows():
-        if letter == row.letter:
-            table.add_row([letter, row.code])
-
-print(table)
+#for letter in word:
+#    for (index, row) in alphabet_data.iterrows():
+#        if letter == row.letter:
+#            table.add_row([letter, row.code])
+#
+#print(table)
 
 #2
 
 
-phonetic_dict = {row.letter:row.code for (index,row) in alphabet_data.iterrows()}
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+while True:
+    word = input("Enter a word: ").upper()
+    phonetic_dict = {row.letter:row.code for (index,row) in alphabet_data.iterrows()}
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print('Please enter letters only (A–Z).')
+    else:
+        print(output_list)
+        break
