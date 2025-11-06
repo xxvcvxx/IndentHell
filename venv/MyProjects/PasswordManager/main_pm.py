3e097d257ff5a9ac1d0b11bc69feae71c32ea6a7
Olga <xvcvx.pl@gmail.com>
2025-11-06 23:04:23 +0100

    add JSON read error handling and password search

--- venv/MyProjects/PasswordManager/main_pm.py
+++ venv/MyProjects/PasswordManager/main_pm.py
@@ -1,22 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data={website:{
+        "email": email,
+        "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showerror(title='Error', message="Please fill all fields!")
     else:
         answer = messagebox.askokcancel(title=website, message="Do you want to save these login details?")
         if answer:
-            with open('file.txt', 'a') as f:
-                f.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-                messagebox.showinfo(title= "Success",message="Password has been saved.")
+            try:
+                with open('file.json', 'r') as f:
+                    data=json.load(f)
+            except (FileNotFoundError, json.JSONDecodeError):
+                data = {}
+            with open('file.json', 'w') as f:
+                data.update(new_data)
+                json.dump(data,f,indent=4)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            messagebox.showinfo(title= "Success",message="Password has been saved.")
 
 
 def password_generate():
@@ -41,6 +53,19 @@ def password_generate():
 
     password_entry.insert(0, password)
 
+def find_password():
+    website = website_entry.get()
+    try:
+        with open('file.json', 'r') as f:
+            data=json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        messagebox.showerror(title='Error', message="File not found or is corrupted.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Website: {website}\nEmail= {email} \nPassword={password}")
+
 
 window = Tk()
 window.title("Password Manager")
@@ -54,9 +79,9 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website: ")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=50)
+website_entry = Entry(width=32)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2, sticky="w", padx=5, pady=5)
+website_entry.grid(row=1, column=1, sticky="w", padx=5, pady=5)
 
 email_label = Label(text="Email/UserName")
 email_label.grid(row=2, column=0, sticky="w", padx=5, pady=5)
@@ -76,5 +101,8 @@ gen_password_button.grid(row=3, column=2, sticky="w")
 
 add_button = Button(text="Add", width=40, command=save)
 add_button.grid(row=4, column=1, columnspan=2, pady=10, sticky="we")
+
+search_button = Button(text='Search', bg= '#ADD8E6',fg ='black',width=15,command=find_password)
+search_button.grid(row=1, column=2, sticky="w")
 
 window.mainloop()
