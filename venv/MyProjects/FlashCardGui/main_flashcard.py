c69d5a56677295d3974ed8f994435c4526586fb1
Olga <xvcvx.pl@gmail.com>
2025-11-16 21:01:48 +0100

    Add initial UI and basic logic

--- venv/MyProjects/FlashCardGui/main_flashcard.py
+++ venv/MyProjects/FlashCardGui/main_flashcard.py
@@ -1,14 +1,36 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/polish_words.csv")
+to_learn = data.to_dict(orient="record")
+print(to_learn)
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title,text="Polish")
+    canvas.itemconfig(card_word, text=current_card["Polish"])
+
 
 window = Tk()
 window.title("FlashCards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
 canvas = Canvas(bg=BACKGROUND_COLOR, highlightthickness=0, width=800, height=600)
 image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,300,image=image)
-canvas.grid(row=1, column=1)
+canvas.grid(row=1, column=0,columnspan=2)
+card_title = canvas.create_text(400, 200, text="", font=("Arial", 40, "italic"), fill="gray")
+card_word = canvas.create_text(400, 300, text="", font=("Arial", 60), fill="black")
+
+btn_right_image = PhotoImage(file="images/right.png")
+btn_right= Button(image=btn_right_image,borderwidth=0, highlightthickness=0,command=next_card)
+btn_right.grid(row=2,column=0)
+
+btn_wrong_image = PhotoImage(file="images/wrong.png")
+btn_wrong = Button(image=btn_wrong_image,borderwidth=0, highlightthickness=0,command=next_card)
+btn_wrong.grid(row=2, column=1,)
 
 window.mainloop()
