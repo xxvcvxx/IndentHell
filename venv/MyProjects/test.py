98f86d7f972bc9996b465ed63b23dcf8af346130
Olga <xvcvx.pl@gmail.com>
2025-11-28 22:53:44 +0100

    api

--- venv/MyProjects/test.py
+++ venv/MyProjects/test.py
@@ -1,1 +1,16 @@
+import requests
+
+parameters =  {
+    "lat":50.0614,
+    "lng": 19.9366,
+}
+URL = 'https://api.sunrise-sunset.org/json'
+
+URL2 = 'https://dummyjson.com/products/1'
+x = requests.get(URL, params= parameters)
+data = x.json()
+print(data['results']['sunrise'])
+print(data['results']['sunset'])
+
+
 
